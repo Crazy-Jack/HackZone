0992dffa64511593253c6e1fbc21dc183c3c278f
Tianqin Li <jacklitianqin@gmail.com>
2019-11-10 19:10:56 -0500

    finished framework of the protfolio

---
+++ protfolio/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Layout, Header, Navigation, Content, Drawer} from 'react-mdl';
+import Main from './components/main';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="demo-big-content">
+            <Layout>
+                <Header title="Title" scroll>
+                    <Navigation>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                        <a href="#">Link</a>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main/>
+                </Content>
+            </Layout>
+        </div>
+    );
+  }
+}
+
+export default App;
